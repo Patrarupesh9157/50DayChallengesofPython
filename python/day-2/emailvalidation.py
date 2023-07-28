e43adcb98fc95128b384bf6852026bfebd8ad5c2
Patrarupesh9157 <patrarupesh9157@gmail.com>
2023-07-29 00:39:42 +0530

    2nd day as email validation program

---
+++ python/day-2/emailvalidation.py
@@ -0,0 +1,29 @@
+email = input('Enater your Email address : ')
+k= 0
+j = 0
+d=0
+if len(email) >= 6:
+    if email[0].isalpha():
+        if ('@' in email) and email.count('@') >=1:
+            for i in email:
+                if i==i.isspace():
+                    k=1
+                elif i.isalpha():
+                    if i==i.upper():
+                        j=1
+                elif i.isdigit():
+                    continue 
+                elif i=="_" or i=="." or i== "@":
+                    continue
+                else :
+                    d=1
+            if k== 1 or j== 1:
+                print('Wrong Email')
+            else :
+                print('Your email is correct')
+        else :
+            print('Wrong Email')
+    else :
+        print('Wrong Email')
+else :
+    print('Wrong Email')
